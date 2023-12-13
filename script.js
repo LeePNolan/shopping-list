0436a30db9575d8d2eaae6673231ff316541eacc
LeePNolan <lpnwebdev@gmail.com>
2023-12-13 22:02:03 +0000

    features - clear and remove buttons, check UI state

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 const itemForm = document.getElementById("item-form");
 const itemInput = document.getElementById("item-input");
 const itemList = document.getElementById("item-list");
+const clearBtn = document.getElementById("clear");
+const itemFilter = document.getElementById("filter");
 
 function addItem(e) {
   e.preventDefault();
@@ -20,6 +22,9 @@ function addItem(e) {
   li.appendChild(button);
 
   itemList.appendChild(li);
+
+  checkUI();
+
   itemInput.value = "";
 }
 
@@ -37,5 +42,34 @@ function createIcon(classes) {
   return icon;
 }
 
+// using event delegation, we check that the clicked dom element contains the remove-item class to ensure the delete button is being clicked, and then remove the parent of it's parent - i.e. the whole list item
+function removeItem(e) {
+  if (e.target.parentElement.classList.contains("remove-item")) {
+    e.target.parentElement.parentElement.remove();
+  }
+  checkUI();
+}
+
+// this function uses a while loop that says "while the list at least has a first child, remove that first child" which of course removes all of the items until none are left
+function clearItems() {
+  while (itemList.firstChild) {
+    itemList.removeChild(itemList.firstChild);
+  }
+  checkUI();
+}
+
+function checkUI() {
+  const items = itemList.querySelectorAll("li");
+  if (items.length === 0) {
+    clearBtn.style.display = "none";
+    itemFilter.style.display = "none";
+  } else {
+    clearBtn.style.display = "block";
+    itemFilter.style.display = "block";
+  }
+}
+
 // Event Listeners
 itemForm.addEventListener("submit", addItem);
+itemList.addEventListener("click", removeItem);
+clearBtn.addEventListener("click", clearItems);
